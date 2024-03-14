aa8898891974d779fb2e98783cf0f92e20b29dbe
neandercode <josephjones555@yahoo.com>
2024-03-14 05:37:30 -0400

    more anime arrays

---
+++ Arrays.java
@@ -0,0 +1,8 @@
+public class Arrays {
+    public static void main(String[] args) {
+        String[] anime = {"Solo Leveling", "Black Clover", "Bungo Stray Dogs", "One Piece", "Dragon Ball Z"};
+        for(int i = 0; i < anime.length; i++) {
+            System.out.println(anime[i]);
+        }
+    }
+}
