1987577b457c634b71ccb44d129c0d08fb253939
neandercode <josephjones555@yahoo.com>
2024-03-15 05:08:35 -0400

    another way of using For Each loop

---
+++ Fer.java
@@ -0,0 +1,8 @@
+public class Fer {
+    public static void main(String[] args) {
+        String[] food = {"Italian", "Indian", "Jamaican", "Mexican"};
+        for (String i : food) {
+            System.out.println(i);
+        }
+    }
+}
