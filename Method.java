2204cb99794f21855740fd5ffd722c092033b2b3
neandercode <josephjones555@yahoo.com>
2024-03-16 06:10:03 -0400

    finally some meat and potatoes

---
+++ Method.java
@@ -0,0 +1,10 @@
+public class Method {
+    static void myMethod() {
+        System.out.println("Look at me, I'm printed out");
+    }
+    public static void main(String [] args) {
+        myMethod();
+        myMethod();
+        myMethod();
+    }
+}
