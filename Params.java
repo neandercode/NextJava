5340a0d789d543e31897cf12978dd0b1fcf520c3
neandercode <josephjones555@yahoo.com>
2024-03-16 06:20:34 -0400

    Brothers

---
+++ Params.java
@@ -0,0 +1,9 @@
+public class Params {
+    static void myMethod(String fname) {
+        System.out.println(fname + " Jaeger");
+    }
+    public static void main(String[] args) {
+        myMethod("Eren");
+        myMethod("Zeke");
+    }
+}
