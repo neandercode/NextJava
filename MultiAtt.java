b8ae9e407cd18f5e585844e430472b4b4d3e7aea
neandercode <josephjones555@yahoo.com>
2024-03-20 05:10:32 -0400

    multiple attributes

---
+++ MultiAtt.java
@@ -0,0 +1,12 @@
+public class MultiAtt {
+    String fname = "Akira";
+    String lname = "Toriyama";
+    int age = 68;
+
+    public static void main(String[] args){
+        MultiAtt myObj = new MultiAtt();
+        System.out.println("Name: " + myObj.fname + " " + myObj.lname);
+        System.out.println("age: " + myObj.age);
+    }
+    
+}
