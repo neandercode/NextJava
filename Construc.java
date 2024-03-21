814790f2494215ab4aa4e289bc1d2aafc498dd5a
neandercode <josephjones555@yahoo.com>
2024-03-21 05:31:30 -0400

    Construct

---
+++ Construc.java
@@ -0,0 +1,12 @@
+public class Construc {
+    int x;
+
+    public Construc(int y) {
+        x = y;
+    }
+
+    public static void main(String[] args){
+        Construc myObj = new Construc(5);
+        System.out.println(myObj.x);
+    }
+}
