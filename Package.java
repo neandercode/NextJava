fcefadcd6638d5ca127a714efb79ae38edacb17f
neandercode <josephjones555@yahoo.com>
2024-03-22 05:24:15 -0400

    added the comment

--- Package.java
+++ Package.java
@@ -12,3 +12,8 @@ import java.util.Scanner;
         myObj.close();
     }
 }
+
+/*I wanted to add that this one actually took some brain power to figure out.
+ turns out you need to close the scanner once you are done but what nothing
+  tells you is to name the close what you named the scanner. 
+  kept getting messages sayin 'resource leak in is never closed'. */
