aeaf8743b93faa1d538f004583280c5f68750fb8
neandercode <josephjones555@yahoo.com>
2024-03-26 05:02:59 -0400

    enum

--- Main.java
+++ Main.java
@@ -1,15 +1,13 @@
-class OuterClass {
-  int x = 10;
-
-  class InnerClass {
-    int y = 5;
+public class Main { 
+  enum Level {
+    LOW,
+    MEDIUM,
+    HIGH
   }
+
+  public static void main(String[] args) { 
+    Level myVar = Level.MEDIUM; 
+    System.out.println(myVar); 
+  } 
 }
 
-public class Main {
-  public static void main(String[] args) {
-    OuterClass myOuter = new OuterClass();
-    OuterClass.InnerClass myInner = myOuter.new InnerClass();
-    System.out.println(myInner.y + myOuter.x);
-  }
-}
