547abd524779648e9eccfc3ca442cd466c5c1d36
neandercode <josephjones555@yahoo.com>
2024-03-29 04:48:51 -0400

    I love anime

---
+++ ArrList.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class ArrList {
+    public static void main(String[] args) {
+        ArrayList<String> anime = new ArrayList<String>();
+        anime.add("DragonBall");
+        anime.add("One Piece");
+        anime.add("Black Clover");
+        anime.add("Solo Leveling");
+
+        System.out.println(anime);
+    }
+}
