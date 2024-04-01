90d095516c9e6ff1a2dcbb5d2a8153f02b298343
neandercode <josephjones555@yahoo.com>
2024-04-01 04:35:34 -0400

    Linked List

---
+++ LinList.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+public class LinList {
+    public static void main(String[] args) {
+        LinkedList<String> anime = new LinkedList<String>();
+        anime.add("Black Clover");
+        anime.add("Bungo Stray Dogs");
+        anime.add("Zom 100");
+        anime.add("Solo Leveling");
+        System.out.println(anime);
+    }
+}
